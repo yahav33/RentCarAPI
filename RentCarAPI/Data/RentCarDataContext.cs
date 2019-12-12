008724b00862b4396a2ae844fcc1bae547730e90
yahav hadad <yahav.hadad33@gmail.com>
2019-12-12 19:41:19 +0200

    Add project files.

---
+++ RentCarAPI/Data/RentCarDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentCarAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentCarAPI.Data
+{
+    public class RentCarDataContext : DbContext
+    {
+        public RentCarDataContext(DbContextOptions<RentCarDataContext> options): base(options) { }
+        
+        public DbSet<Value> Values { get; set; } 
+    }
+}
